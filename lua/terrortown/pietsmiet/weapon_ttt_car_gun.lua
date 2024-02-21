55f02d2eb446a3daefab669036dacc10c64cf737
Tim Goll <github@timgoll.de>
2024-02-21 11:34:14 +0100

    added sportswäggon sound

--- lua/terrortown/pietsmiet/weapon_ttt_car_gun.lua
+++ lua/terrortown/pietsmiet/weapon_ttt_car_gun.lua
@@ -1,3 +1,19 @@
 if CLIENT then
     PIETSMIET.PrintName = "ps_car_gun"
 end
+
+if SERVER then
+    util.PrecacheSound("pietsmiet/sportswaggon.wav")
+
+    function PIETSMIET:PrimaryAttack()
+        local ply = self:GetOwner()
+
+        timer.Simple(0.6, function()
+            if not IsValid(ply) then
+                return
+            end
+
+            ply:EmitSound("pietsmiet/sportswaggon.wav")
+        end)
+    end
+end
