0de856569d8f4379972abc9638ca0487b4535515
Tim Goll <github@timgoll.de>
2024-05-21 10:00:26 +0200

    boombody: das ist fake

---
+++ lua/terrortown/pietsmiet/weapon_ttt_boom_body.lua
@@ -0,0 +1,15 @@
+if SERVER then
+    util.PrecacheSound("pietsmiet/das_ist_fake.mp3")
+
+    hook.Add("TTT2BoomBodyOnExplode", "PietSmietBoomBodyFake", function(rag)
+        print("boom body exploded, playing sound")
+
+        sound.Play("pietsmiet/das_ist_fake.mp3", rag:GetPos())
+    end)
+
+    function PIETSMIET:PrimaryAttack()
+        local ply = self:GetOwner()
+
+        ply:EmitSound("pietsmiet/das_ist_fake.mp3", 70, 100, 0.25)
+    end
+end
